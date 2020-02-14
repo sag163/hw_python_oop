fde7763dade3d76fe10711515e79af63eb096197
Сергей <sag163@mail.ru>
2020-02-14 16:25:24 +0400

    исправлены замечания после ревью

--- homework.py
+++ homework.py
@@ -8,19 +8,19 @@ class Calculator:
         self.records.append(Record)
         
     def get_week_stats(self):
-        summ7 = 0
+        week_consumption = 0
         timedelta = dt.datetime.now().date() - dt.timedelta(6)
-        for j in self.records:
-            if   timedelta <= j.date <= dt.datetime.now().date():
-                summ7 = summ7 + int(j.amount)
-        return summ7
+        for record in self.records:
+            if timedelta <= record.date <= dt.datetime.now().date():
+                week_consumption = week_consumption + int(record.amount)
+        return week_consumption
 
     def get_today_stats(self):
-        summ = 0
-        for i in range(len(self.records)):
-            if self.records[i].date == (dt.datetime.now()).date():
-                summ += self.records[i].amount
-        return(summ)
+        today_consumption = 0
+        for record in self.records:
+            if record.date == (dt.datetime.now()).date():
+                today_consumption += record.amount
+        return(today_consumption)
 
 class Record:
     def __init__(self, amount, comment, date = ""):
@@ -35,42 +35,32 @@ class CashCalculator(Calculator):
     USD_RATE = 63.34
     EURO_RATE = 69.16 
     def get_today_cash_remained(self, currency):
-        count = 0
-        for i in range(len(self.records)):
-            if self.records[i].date == (dt.datetime.now()).date():
-                count += self.records[i].amount
-        ost = float(self.limit - count) 
+        spent_today = 0
         
-        if currency == 'eur':
-            remainder = ost / self.EURO_RATE
-            currency2 = 'Euro'
-        elif currency == 'usd':
-            remainder = ost / self.USD_RATE
-            currency2 = 'USD'
-        else:
-            remainder = ost
-            currency2 = 'руб'
-
-        if ost > 0:
-            print(ost)
-            return(f'На сегодня осталось {round(remainder, 2)} {currency2}')
-        elif ost == 0:
+        for record in self.records:
+            if record.date == (dt.datetime.now()).date():
+                spent_today += record.amount
+        daily_balance = float(self.limit - spent_today)   
+        euro = abs(round(daily_balance/self.EURO_RATE, 2))   #Много различных вариантов перебирал, но решил использовать такую конструкцию перевода валюты, чтобы не загромождать словарь
+        dollar = abs(round(daily_balance/ self.USD_RATE, 2))
+        ruble = abs(round(daily_balance, 2))
+        dict_money = {'eur':['Euro', euro], 'usd':['USD', dollar], 'rub':['руб', ruble]}
+       
+        if daily_balance > 0:
+            return(f'На сегодня осталось {(dict_money[currency])[1]} {(dict_money[currency])[0]}')
+        elif daily_balance == 0:
             return('Денег нет, держись')
         else:
-            return(f'Денег нет, держись: твой долг - {abs(round(remainder, 2))} {currency2}')
+            return f'Денег нет, держись: твой долг - {(dict_money[currency])[1]} {(dict_money[currency])[0]}'
         
         
 class CaloriesCalculator(Calculator):
     def get_calories_remained(self):
-        count = 0
-        for i in range(len(self.records)):
-            if self.records[i].date == (dt.datetime.now()).date():
-                count += self.records[i].amount 
-        ost = self.limit - count 
-        if count < self.limit and count > 0:
-            N = self.limit - count
-            return(f'Сегодня можно съесть что-нибудь ещё, но с общей калорийностью не более {N} кКал')
+        spent_today = self.get_today_stats()
+        if spent_today < self.limit and spent_today > 0:
+            remains = self.limit - spent_today
+            return(f'Сегодня можно съесть что-нибудь ещё, но с общей калорийностью не более {remains} кКал')
         else:
-            return("Хватит есть!") 
+            return("Хватит есть!")
 
 
